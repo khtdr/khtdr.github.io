e8c3cb2a3b2ab33284b48bac74f4e5e08b548183
Kay Oh <khtdr.com@gmail.com>
2015-03-12 04:42:46 +0000

    new website :)

---
+++ index.js
@@ -0,0 +1,12 @@
+var finalhandler = require('finalhandler')
+  , http         = require('http')
+  , serveStatic  = require('serve-static');
+
+var serve = serveStatic('.', {'index': ['index.html']});
+
+var server = http.createServer(function (req, res) {
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
+
+server.listen(12378);
