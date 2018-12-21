e7f43ae17c3bfae7eb2a221041af5bbc2c2b12a5
Kay Oh <khtdr.com@gmail.com>
2018-12-20 22:03:56 -0700

    updated link

--- dev-server.js
+++ dev-server.js
@@ -5,7 +5,9 @@ var app     = express();
 var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
 
-fs.watch(__dirname, { recursive:true }, function (filename) {
+fs.watch(__dirname, { recursive:true }, (event, name) => {
+  if (name.match(/^(\.git|node_modules)/)) return;
+  console.log(event, name);
   io.emit('file-change');
 });
 
@@ -19,7 +21,7 @@ app.get([/\/$/, /.*\.html$/], function (req, res) {
   filename += filename.endsWith('/')? 'index.html': '';
   fs.readFile(filename, function (err, data) {
     res.send(data
-            + '<script src="/node_modules/socket.io-client/socket.io.js">'
+            + '<script src="/node_modules/socket.io-client/dist/socket.io.js">'
             + '</script>'
             + '<script>'
             + '  var socket = io();'
