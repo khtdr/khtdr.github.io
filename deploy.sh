6a0713c6d36cd483398a1012bd3c6f8b412ef26e
Kay Oh <khtdr.com@gmail.com>
2022-02-14 19:11:30 -0700

    better scripts

--- deploy.sh
+++ deploy.sh
@@ -6,19 +6,13 @@ if [ -n "$*" ]; then
 	msg="$*"
 fi
 
-printf "\033[0;32mPushing changes on hugo branch...\033[0m\n"
+printf "\033[0;32mBuilding static site on master...\033[0m\n"
+hugo -t terminal -d docs
+
+printf "\033[0;32mPublishing updates on master...\033[0m\n"
 git add .
 git commit -m "$msg"
 git push
-
-printf "\033[0;32mBuilding static site on master...\033[0m\n"
-hugo -t terminal
-
-printf "\033[0;32mPublishing updates on master...\033[0m\n"
-cd public
-git add .
-git commit -m "$msg"
-git push origin master
 
 printf "\033[0;32mDone!\033[0m\n"
 cd -
--- run-dev.sh
+++ run-dev.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 concurrently
-npx concurrently --names "HUGO,HTTP" "hugo -t terminal -b http://localhost:8080 -w" "npx static-serve -p 8080 public -o"
+npx concurrently --raw --names "HUGO,HTTP" \
+    "hugo -d .build -t terminal -b http://localhost:8080/ -w" \
+    "npx static-server -p 8080 .build -o"
+
+echo "Don't forget to build and deploy your new changes"
