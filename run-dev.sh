fce0b230ba9653f49e194009c1df286434afede1
joey <khtdr.com@gmail.com>
2023-01-05 11:41:05 -0700

    change for the sake of change

--- run-dev.sh
+++ run-dev.sh
@@ -1,9 +1,1 @@
-#!/usr/bin/env bash
-
-# npx concurrently --raw --names "HUGO,HTTP" \
-#     "hugo -d .build -t hugo-starter-theme -b http://0.0.0.0:8080/ -w" \
-#     "npx static-server -p 8080 .build -o"
-
-hugo server --buildDrafts --buildFuture --port 8080
-
-echo "Don't forget to build and deploy your new changes"
+hugo server --buildDrafts --buildFuture --port 1313 -w
